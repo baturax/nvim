9fa4144c10d045ab296ef30819a208e29598d817
bai <bai@baturax.com>
2024-12-26 04:19:54 +0300

    bai

--- lua/plugins/bloats.lua
+++ lua/plugins/bloats.lua
@@ -1,4 +1,26 @@
 return {
+
+{
+   'williamboman/mason.nvim',
+   opts = {
+      ui = {
+        check_outdated_packages_on_open = true,
+        icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+        }
+    },
+
+   },
+},
+
+
+{
+   'mong8se/actually.nvim',
+   dependencies = 'stevearc/dressing.nvim',
+},
+
 {
     'windwp/nvim-autopairs',
     event = "InsertEnter",
