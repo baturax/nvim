41da618e86fea8b1913d0b684b39a473f76b34a6
Baturax <bai@baturax.com>
2025-09-09 02:14:28 +0300

    added the game

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: undefined-global
 local v = vim
 local o = v.opt
 local g = v.g
@@ -14,30 +15,30 @@ pack.add({
 	{ src = gh .. "nvim-treesitter/nvim-treesitter", branch = "main" },
 	{ src = gh .. "akinsho/bufferline.nvim" },
 	{ src = gh .. "altermo/ultimate-autopair.nvim", branch = "v0.6" },
+  { src = gh .. "ThePrimeagen/vim-be-good" }
 })
 
 
 local function on_attach(client, bufnr)
-  lsp.completion.enable(true, client.id, bufnr, {
-    autotrigger = true,
-    convert = function(item)
-      return { abbr = item.label:gsub("%b()", "") }
-    end,
-  })
+	lsp.completion.enable(true, client.id, bufnr, {
+		autotrigger = true,
+		convert = function(item)
+			return { abbr = item.label:gsub("%b()", "") }
+		end,
+	})
 
-  key("i", "<C-space>", lsp.completion.get, { desc = "trigger autocompletion" })
-  api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", { noremap = true, silent = true })
+	key("i", "<C-space>", lsp.completion.get, { desc = "trigger autocompletion" })
+	api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", { noremap = true, silent = true })
 end
 
 local servers = { "gopls", "basedpyright", "lua_ls" }
 
 for _, server in ipairs(servers) do
-  lsp.enable(server)
-  lsp.config(server, {
-    on_attach = on_attach,
-  })
+	lsp.enable(server)
+	lsp.config(server, {
+		on_attach = on_attach,
+	})
 end
-
 
 -- Plugins
 --
@@ -113,9 +114,8 @@ key("i", "<Up>", function()
 	return fn.pumvisible() == 1 and "<C-p>" or "<Up>"
 end, { expr = true, silent = true })
 
-
 key("n", "<leader>d", function()
-  diagnostic.open_float(0, { focusable = false }) -- sadece popup
+	diagnostic.open_float(0, { focusable = false }) -- sadece popup
 end)
 
 key("n", "<leader>ca", lsp.buf.code_action, { noremap = true, silent = true })
@@ -123,17 +123,17 @@ key("n", "<leader>ca", lsp.buf.code_action, { noremap = true, silent = true })
 -- Diag
 
 diagnostic.config({
-  virtual_text = true,
-  signs = true,
-  underline = true,
-  update_in_insert = false,
-  severity_sort = true,
+	virtual_text = true,
+	signs = true,
+	underline = true,
+	update_in_insert = false,
+	severity_sort = true,
 })
 
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+	local hl = "DiagnosticSign" .. type
+	fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
 
 -- Functions
@@ -147,3 +147,4 @@ api.nvim_create_autocmd({ "BufReadPost" }, {
 		api.nvim_exec('silent! normal! g`"zv', false)
 	end,
 })
+
