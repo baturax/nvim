9392aa980c56b16e15f50bb5de3301a96b1cb225
Baturax <bai@baturax.com>
2025-09-19 01:54:49 +0300

    golang

--- init.lua
+++ init.lua
@@ -221,6 +221,32 @@ function _G.MyWinbar()
 end
 
 -- Lsp Configs
+-- go
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*.go",
+	callback = function()
+local base_params = vim.lsp.util.make_range_params(nil, "utf-8")
+local params = {
+  textDocument = base_params.textDocument,
+  range = base_params.range,
+  context = { only = { "source.organizeImports" } }
+}
+		params.context = { only = { "source.organizeImports" } }
+		local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+		for cid, res in pairs(result or {}) do
+			for _, r in pairs(res.result or {}) do
+				if r.edit then
+					local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+					vim.lsp.util.apply_workspace_edit(r.edit, enc)
+				end
+			end
+		end
+		vim.lsp.buf.format({ async = false })
+	end
+})
+
+
 -- lua
 lspc("lua_ls", {
 	on_init = function(client)
